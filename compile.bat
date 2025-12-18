44d157d018aa3ee757f0997247ad84220b3fff6d
Joshua O'Gorman <141955352+Skellate@users.noreply.github.com>
2025-12-18 09:28:16 +0200

    Delete compile.bat

--- compile.bat
+++
@@ -1,3 +0,0 @@
-C:\VulkanSDK\1.4.321.1\Bin\glslc.exe shaders\simpleShader.vert -o shaders\simpleShader.vert.spv
-C:\VulkanSDK\1.4.321.1\Bin\glslc.exe shaders\simpleShader.frag -o shaders\simpleShader.frag.spv
-pause
